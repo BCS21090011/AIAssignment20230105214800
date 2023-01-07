17a352433df1a8a45360c035708f9554c348399b
BCS21090011 <bcs21090011@student.bcs.edu.my>
2023-01-07 11:35:13 +0800

    Remove the data hints (not all, some are needed, and the return type function is kept) because may cause some errors.

--- main.py
+++ main.py
@@ -8,22 +8,22 @@ from sklearn.metrics import confusion_matrix
 import seaborn as sbrn
 
 class ProcessDataset():
-    def __init__(self, filepath: str):
-        self.FileName: str = filepath
-        self.OriData: pd.DataFrame = pd.read_csv(filepath)
-        self.ColsNeeded: list[str] = ["Hospt", "Treat", "Outcome", "AcuteT", "Age", "Gender"]
-        self.NumericCols: list[str] = ["Hospt", "AcuteT", "Age", "Gender"]
+    def __init__(self, filepath):
+        self.FileName = filepath
+        self.OriData = pd.read_csv(filepath)
+        self.ColsNeeded = ["Hospt", "Treat", "Outcome", "AcuteT", "Age", "Gender"]
+        self.NumericCols = ["Hospt", "AcuteT", "Age", "Gender"]
 
         self.ValidataFile()
         if self.FileValid == True:
             self.Process()
 
     def ValidataFile(self)->bool:
-        self.FileValid: bool = False
+        self.FileValid = False
 
         if set(self.ColsNeeded).issubset(self.OriData.columns):
-            self.OutcomeValValid: bool = False
-            self.NumericColsValValid: bool = True
+            self.OutcomeValValid = False
+            self.NumericColsValValid = True
 
             if self.OriData["Outcome"].unique().all() in ["No Recurrence", "Recurrence"]:
                 self.OutcomeValValid = True
@@ -40,7 +40,7 @@ class ProcessDataset():
         self.DropUnneededCols()
 
         # Mapping:
-        self.OutcomeMap: dict = {"No Recurrence": 0, "Recurrence": 1}
+        self.OutcomeMap = {"No Recurrence": 0, "Recurrence": 1}
         self.Data["Outcome"] = self.Data["Outcome"].map(self.OutcomeMap)
 
         # Somehow one hot encoding(?):
@@ -49,8 +49,8 @@ class ProcessDataset():
         self.Data = self.Data.join(self.TreatColDum)
 
         # Defining output column and features:
-        self.OutCol: str = "Outcome"
-        self.Features: list[str] = self.Data.columns.tolist()
+        self.OutCol = "Outcome"
+        self.Features = self.Data.columns.tolist()
         self.Features.remove(self.OutCol)
 
         # Setting X and y:
@@ -61,35 +61,35 @@ class ProcessDataset():
         self.Xtrn, self.Xtst, self.ytrn, self.ytst = train_test_split(self.X, self.y, test_size=0.25, random_state=0)
 
         # Setting decision tree:
-        self.Classifier: DecisionTreeClassifier = DecisionTreeClassifier(criterion="entropy", random_state=0)
+        self.Classifier = DecisionTreeClassifier(criterion="entropy", random_state=0)
         self.Classifier.fit(self.Xtrn, self.ytrn)
 
         # Confusion matrix:
         self.yPred = self.Classifier.predict(self.Xtst)
-        self.CM: confusion_matrix = confusion_matrix(self.ytst, self.yPred)
+        self.CM = confusion_matrix(self.ytst, self.yPred)
 
     def DropUnneededCols(self):
         unNeededBoolArray = ~(self.OriData.columns.isin(self.ColsNeeded))
-        self.Data: pd.DataFrame = self.OriData.drop(self.OriData.columns[unNeededBoolArray], axis=1)
+        self.Data = self.OriData.drop(self.OriData.columns[unNeededBoolArray], axis=1)
 
-    def Predict(self, inputs: list)->int:
-        Xinputs: list[list] = [inputs]
-        pred: int = self.Classifier.predict(Xinputs)[0]
+    def Predict(self, inputs)->int:
+        Xinputs = [inputs]
+        pred = self.Classifier.predict(Xinputs)[0]
 
         return pred
 
-    def CMSaveFig(self, fileName: str):
+    def CMSaveFig(self, fileName):
         sbrn.heatmap(self.CM, annot=True)
         plt.savefig(fileName)
         plt.clf()
 
-    def DTSaveFig(self, saveName: str):
+    def DTSaveFig(self, saveName):
         tree.plot_tree(self.Classifier, feature_names=self.Features)
         plt.savefig(saveName)
         plt.clf()
 
     def __str__(self)->str:
-        strOutput: str = f"FileName: {self.FileName: ^16} Valid: {self.FileValid}"
+        strOutput = f"FileName: {self.FileName: ^16} Valid: {self.FileValid}"
         return strOutput
 
 # ________________Interface________________
@@ -99,12 +99,12 @@ from tkinter import filedialog
 from tkinter import messagebox
 from PIL import ImageTk, Image
 
-fileValid: bool = False
-inputsValid: bool = False
-TreatColVals: list[str] = ["Nothing"]
+fileValid = False
+inputsValid = False
+TreatColVals = ["Nothing"]
 de: ProcessDataset
 
-def ValidLabelOutput(lbl: tk.Label, valid: bool, oriText: str, invalidText: str=None):
+def ValidLabelOutput(lbl, valid, oriText, invalidText=None):
     if valid == True:
         lbl["text"] = oriText
     else:
@@ -116,14 +116,14 @@ def ValidLabelOutput(lbl: tk.Label, valid: bool, oriText: str, invalidText: str=
 def PredButton_OnClick():
     ValidLabelOutput(BrowseFileLabel, fileValid, "Browse file")
     if fileValid == True:
-        HosptValStr: str = HosptEntry.get()
-        AcuteTValStr: str = AcuteTEntry.get()
-        AgeValStr: str = AgeEntry.get()
-        GenderValStr: str = GenderStrVar.get()
+        HosptValStr = HosptEntry.get()
+        AcuteTValStr = AcuteTEntry.get()
+        AgeValStr = AgeEntry.get()
+        GenderValStr = GenderStrVar.get()
 
-        HosptValValid: bool = ValidStrInputIsIntNonNeg(HosptValStr)
-        AcuteTValValid: bool = ValidStrInputIsIntNonNeg(AcuteTValStr)
-        AgeValValid: bool = ValidStrInputIsIntNonNeg(AgeValStr)
+        HosptValValid = ValidStrInputIsIntNonNeg(HosptValStr)
+        AcuteTValValid = ValidStrInputIsIntNonNeg(AcuteTValStr)
+        AgeValValid = ValidStrInputIsIntNonNeg(AgeValStr)
 
         ValidLabelOutput(HosptLabel, HosptValValid, "Hospital ID:")
         ValidLabelOutput(AcuteTLabel, AcuteTValValid, "Depressed Time (days):")
@@ -133,28 +133,28 @@ def PredButton_OnClick():
             global inputsValid
             inputsValid = True
 
-            genderValsDict: dict = {"Male": 2, "Female": 1}
+            genderValsDict = {"Male": 2, "Female": 1}
 
-            Hospt: int = int(HosptValStr)
-            AcuteT: float = float(AcuteTValStr)
-            Age: int = int(AgeValStr)
-            Gender: int = int(genderValsDict[GenderValStr])
-            Treat: str = TreatStrVar.get()
+            Hospt = int(HosptValStr)
+            AcuteT= int(AcuteTValStr)
+            Age = int(AgeValStr)
+            Gender = int(genderValsDict[GenderValStr])
+            Treat = TreatStrVar.get()
 
-            TreatValsDict: dict = {val: 0 for val in TreatColVals}
+            TreatValsDict = {val: 0 for val in TreatColVals}
             TreatValsDict[Treat] = 1
 
-            userInputsDict: dict = {"Hospt": Hospt, "AcuteT": AcuteT, "Age": Age, "Gender": Gender}
+            userInputsDict = {"Hospt": Hospt, "AcuteT": AcuteT, "Age": Age, "Gender": Gender}
             userInputsDict.update(TreatValsDict)
 
-            userInputs: list = []
+            userInputs = []
 
             for feat in de.Features:
                 userInputs.append(userInputsDict[feat])
 
-            predResult: int = de.Predict(userInputs)
-            reversedOutcomeMap: dict = {v: k for k, v in de.OutcomeMap.items()}
-            predResultStr: str = reversedOutcomeMap[predResult]
+            predResult = de.Predict(userInputs)
+            reversedOutcomeMap = {v: k for k, v in de.OutcomeMap.items()}
+            predResultStr = reversedOutcomeMap[predResult]
 
             messagebox.showinfo(title="Prediction result:", message=f"Prediction result: {predResultStr:^16}")
 
@@ -162,7 +162,7 @@ def BrowseFileButton_OnClick():
     global fileValid
     global de
 
-    fileName: str = filedialog.askopenfilename(title="Select a hospital data file (.csv)", filetypes=[("CSV files", "*.csv*")])
+    fileName = filedialog.askopenfilename(title="Select a hospital data file (.csv)", filetypes=[("CSV files", "*.csv*")])
 
     if fileName == '':
         fileValid = False
@@ -178,13 +178,13 @@ def BrowseFileButton_OnClick():
             de.CMSaveFig("CMatrix.png")
             de.DTSaveFig("DTree.png")
             de.DTSaveFig("DTree.svg")
-            photo: ImageTk.PhotoImage = ImageTk.PhotoImage(Image.open("DTree.png"))
+            photo = ImageTk.PhotoImage(Image.open("DTree.png"))
             DiagramPanel.config(image=photo)
             DiagramPanel.image = photo
 
     ValidLabelOutput(BrowseFileLabel, fileValid, "Browse file")
 
-def ResetOptionMenuOptions(optMenu: tk.OptionMenu, strVar: tk.StringVar, newOptions: list[str], defaultValIndex: int=None):
+def ResetOptionMenuOptions(optMenu, strVar, newOptions, defaultValIndex=None):
     menu = optMenu["menu"]
     menu.delete(0, "end")
 
@@ -199,9 +199,9 @@ def ResetOptionMenuOptions(optMenu: tk.OptionMenu, strVar: tk.StringVar, newOpti
 
     strVar.set(newOptions[defaultValIndex])
 
-def ValidStrInputIsFloatNonNeg(userInput: str)->bool:
+def ValidStrInputIsFloatNonNeg(userInput)->bool:
     try:
-        inVal: float = float(userInput)
+        inVal = float(userInput)
 
         # Codes below won't run if float(userInput) have an error:
         # Check if user input is negative:
@@ -213,32 +213,32 @@ def ValidStrInputIsFloatNonNeg(userInput: str)->bool:
     except ValueError:
         return False
 
-def ValidStrInputIsIntNonNeg(userInput: str)->bool:
+def ValidStrInputIsIntNonNeg(userInput)->bool:
     if userInput.find('.') == -1:
         return ValidStrInputIsFloatNonNeg(userInput)
     else:
         return False
 
-def ValidEntryValFloatNonNeg(userInput: str)->bool:
-    valid: bool = ValidStrInputIsFloatNonNeg(userInput)
+def ValidEntryValFloatNonNeg(userInput)->bool:
+    valid = ValidStrInputIsFloatNonNeg(userInput)
 
     if userInput == '':
         valid = True
 
     return valid
 
-def ValidEntryValIntNonNeg(userInput: str)->bool:
-    valid: bool = ValidStrInputIsIntNonNeg(userInput)
+def ValidEntryValIntNonNeg(userInput)->bool:
+    valid = ValidStrInputIsIntNonNeg(userInput)
 
     if userInput == '':
         valid = True
 
     return valid
 
-def CustomEntry(root, validateCommand, entryDefaultVal: str=None)->tk.Entry:
-    entry: tk.Entry = tk.Entry(root, justify="center")
+def CustomEntry(root, validateCommand, entryDefaultVal=None)->tk.Entry:
+    entry = tk.Entry(root, justify="center")
     entry.config(validate="key", validatecommand=(entry.register(validateCommand), '%P'))
-    entryTxtVar: tk.StringVar = tk.StringVar(root)
+    entryTxtVar = tk.StringVar(root)
 
     if entryDefaultVal != None:
         if validateCommand(entryDefaultVal) == True:
@@ -247,60 +247,60 @@ def CustomEntry(root, validateCommand, entryDefaultVal: str=None)->tk.Entry:
 
     return entry
 
-def CustomOptionMenu(root, options: list[str], defaultValIndex: int=None)->(tk.OptionMenu, tk.StringVar):
+def CustomOptionMenu(root, options, defaultValIndex=None)->(tk.OptionMenu, tk.StringVar):
     if defaultValIndex == None:
         defaultValIndex = 0
 
     if (defaultValIndex >= len(options)) | (defaultValIndex < 0):
         defaultValIndex = 0
 
-    defaultVal: tk.StringVar = tk.StringVar(root)
+    defaultVal = tk.StringVar(root)
     defaultVal.set(options[defaultValIndex])
-    optMenu: tk.OptionMenu = tk.OptionMenu(root, defaultVal, *options)
+    optMenu = tk.OptionMenu(root, defaultVal, *options)
 
     return (optMenu, defaultVal)
 
-root: tk.Tk = tk.Tk()
+root = tk.Tk()
 root.title("Depression Treatment Prediction")
 root.geometry("800x700")
 root.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9), weight=0)
 root.grid_columnconfigure((0, 1, 2), weight=1)
 
-BrowseFileLabel: tk.Label = tk.Label(root, text="Browse file", justify="center")
+BrowseFileLabel = tk.Label(root, text="Browse file", justify="center")
 BrowseFileLabel.grid(column=0, row=0)
-BrowseFileButton: tk.Button = tk.Button(root, text="Browse file", command=BrowseFileButton_OnClick)
+BrowseFileButton = tk.Button(root, text="Browse file", command=BrowseFileButton_OnClick)
 BrowseFileButton.grid(column=2, row=0)
 
-HosptLabel: tk.Label = tk.Label(root, text= "Hospital ID:", justify="center")
+HosptLabel = tk.Label(root, text= "Hospital ID:", justify="center")
 HosptLabel.grid(column=0, row=1)
-HosptEntry: tk.Entry = CustomEntry(root, ValidEntryValIntNonNeg)
+HosptEntry = CustomEntry(root, ValidEntryValIntNonNeg)
 HosptEntry.grid(column=2, row=1)
 
-AcuteTLabel: tk.Label = tk.Label(root, text="Depressed Time (days):", justify="center")
+AcuteTLabel = tk.Label(root, text="Depressed Time (days):", justify="center")
 AcuteTLabel.grid(column=0, row=2)
-AcuteTEntry: tk.Entry = CustomEntry(root, ValidEntryValIntNonNeg, "0")
+AcuteTEntry = CustomEntry(root, ValidEntryValIntNonNeg, "0")
 AcuteTEntry.grid(column=2, row=2)
 
-AgeLabel: tk.Label = tk.Label(root, text="Age:", justify="center")
+AgeLabel = tk.Label(root, text="Age:", justify="center")
 AgeLabel.grid(column=0, row=3)
-AgeEntry: tk.Entry = CustomEntry(root, ValidEntryValIntNonNeg)
+AgeEntry = CustomEntry(root, ValidEntryValIntNonNeg)
 AgeEntry.grid(column=2, row=3)
 
-GenderLabel: tk.Label = tk.Label(root, text="Gender:", justify="center")
+GenderLabel = tk.Label(root, text="Gender:", justify="center")
 GenderLabel.grid(column=0, row=4)
-genderVals: list[str] = ["Male", "Female"]
+genderVals = ["Male", "Female"]
 GenderOptMenu, GenderStrVar = CustomOptionMenu(root, genderVals)
 GenderOptMenu.grid(column=2, row=4)
 
-TreatLabel: tk.Label = tk.Label(root, text="Treat:", justify="center")
+TreatLabel = tk.Label(root, text="Treat:", justify="center")
 TreatLabel.grid(column=0, row=5)
 TreatOptMenu, TreatStrVar = CustomOptionMenu(root, TreatColVals)
 TreatOptMenu.grid(column=2, row=5)
 
-PredButton: tk.Button = tk.Button(root, text="Predict", command=PredButton_OnClick)
+PredButton = tk.Button(root, text="Predict", command=PredButton_OnClick)
 PredButton.grid(column=1, row=6)
 
-DiagramPanel: tk.Label = tk.Label(root)
+DiagramPanel = tk.Label(root)
 DiagramPanel.grid(column=0, columnspan=3, row=7, rowspan=3)
 
 root.mainloop()
